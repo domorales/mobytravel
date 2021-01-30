ad65e3689147e253082f12c7ab4bbdc4d7fa545a
domorales <domorale@espol.edu.ec>
2021-01-30 13:07:36 -0500

    nuevas modificaciones

--- js/scroll.js
+++ js/scroll.js
@@ -2,27 +2,27 @@ window.addEventListener('scroll' , function(){
 
     let dato;
     let posicion;
-    let heightWindow = screen.height;
+    let heightWindow = screen.height-40;
     let widthWindow = screen.width;
 
     
     dato = document.getElementById("info");
     posicion = dato.getBoundingClientRect().top;
-    if(posicion<heightWindow) dato.style.animation = "fadeinleft 1.5s  ease-out";
+    if(posicion<heightWindow && widthWindow>992) dato.style.animation = "fadeinleft 1.5s  ease-out";
 
 
     dato = document.getElementById("tour");
     posicion = dato.getBoundingClientRect().top;
-    if(posicion<heightWindow) dato.style.animation = "fadein 2s";
+    if(posicion<heightWindow && widthWindow>992) dato.style.animation = "fadein 2s";
 
 
     dato = document.getElementById("cards");
     posicion = dato.getBoundingClientRect().top;
-    if(posicion<heightWindow) dato.style.animation = "fadeinUp 1.5s";
+    if(posicion<heightWindow && widthWindow>992) dato.style.animation = "fadeinUp 1.5s";
 
     dato = document.getElementById("contact");
     posicion = dato.getBoundingClientRect().top;
-    if(posicion<heightWindow) dato.style.animation = "fadeinleft 1.3s  ease-out";
+    if(posicion<heightWindow && widthWindow>992) dato.style.animation = "fadeinleft 1.3s  ease-out";
 
 
 
